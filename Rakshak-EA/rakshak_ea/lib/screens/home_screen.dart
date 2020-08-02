f22d1120058f64f469544ffdf82b4aadb7e923de
Ritik Harchani <harchani.ritik@gmail.com>
2020-08-02 21:23:53 +0530

    This is where it all started

---
+++ Rakshak-EA/rakshak_ea/lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:rakshak_ea/constants.dart';
+import 'package:rakshak_ea/screens/auth_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomeScreen extends StatefulWidget {
+  final String name;
+  final String officerId;
+  final String phoneNo;
+
+  HomeScreen({this.name,this.officerId,this.phoneNo});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async{
+            //TODO: Sign Out
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            prefs.remove(kPrefLoggedIn);
+            prefs.remove(kPrefName);
+            prefs.remove(kPrefOfficeId);
+            prefs.remove(kPrefPhoneNo);
+            Navigator.pushReplacement(context, MaterialPageRoute(
+              builder: (context)=>AuthScreen(),
+            ));
+          },
+        ),
+        body: Center(child: Text('Welcome to Home Page')),
+    );
+  }
+}
